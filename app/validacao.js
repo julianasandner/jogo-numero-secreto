09eb66de731d4d78bebe47a43bd3680f6929140f
Juliana Sandner <102472270+julianasandner@users.noreply.github.com>
2023-01-13 21:19:33 -0300

    Publicando o jogo

---
+++ app/validacao.js
@@ -0,0 +1,54 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+
+        elementoChute.innerHTML += `
+        <div>Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+
+        document.body.innerHTML = `
+            <h2>Você Acertou!!!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
+    } else {
+
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>`
+    }
+    
+}
+
+function chuteForInvalido(numero) {
+
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+
+    if (e.target.id == 'jogar-novamente') {
+
+        window.location.reload()
+    }
+})
